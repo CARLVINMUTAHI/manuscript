0564e557626976c8c58cd69a7b2a74d1bd9763fa
mainamuriuki274 <43462574+mainamuriuki274@users.noreply.github.com>
2018-10-03 08:50:04 +0300

    Add files via upload

---
+++ Manuscript readers module/reader.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>MANUSCRIPT RANKING</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <style>
+ #navbar {
+    background-color: white; /* Black background color */
+    position: fixed; /* Make it stick/fixed */
+    top: 0; /* Stay on top */
+    width: 100%; /* Full width */
+    transition: top 0.3s; /* Transition effect when sliding down (and up) */
+border-bottom: 2px solid rgba(230,115,0,0.6);
+	}
+
+
+/* Style the navbar links */
+#navbar a {
+    float: right;
+    display: block;
+    color: black;
+	font-size:20px;
+    text-align: center;
+    padding: 15px;
+    text-decoration: none;
+	padding-right:350px;
+}
+
+#navbar a:hover {
+    background-color: #ddd;
+    color: black;
+} 
+ .left{
+ height:100%;
+ float:left;
+ width:23%;
+ background-color:red;
+ position:fixed;
+ }
+ .category
+ {
+ background-color:#2E2928;
+ width:100%;
+ height:85%;
+ color:#989796;
+ }
+ .right{
+ float:right;
+ background-color:white;
+ width:77%;
+ height:100opx;
+ }
+ body{
+	margin: 0;
+	padding: 0;
+	background-color:white;
+	color: black;
+	font-family: Arial;
+	font-size: 15px;
+	overflow-x:hidden;
+	background-attachment:cover;
+
+}
+.nav{
+background-color:#E93E00;
+ width:100%;
+  margin-top:-20px;
+  padding-top:30px;
+ height:15%;
+
+ text-align:center;
+}
+.nav input[type=text]{
+	width: 85%;
+	height: 18px;
+	background-color:white;
+	border: 1px solid rgba(230,115,0,0.6);
+	border-radius: 15px;
+	color: black;
+	font-family: 'Exo', sans-serif;
+	font-size: 14px;
+	font-weight: 400;
+	padding: 4px;
+	padding-left:30px;
+
+}
+
+</style>
+</head>
+<body>
+<div class="left">
+<div class="nav">
+<p style="color:white;">MANUSCRIPT RANKING</p>
+<input type="text" placeholder="Search Books and Authors">
+</div>
+<div class="category">
+<br>
+<p style="color:white;">Categories</p>
+
+<input type="checkbox" name="category" value="All">All<br>
+<input type="checkbox" name="category" value="Science fiction">Science fiction<br>
+<input type="checkbox" name="category" value="Romance">Romance<br>
+<input type="checkbox" name="category" value="Drama">Drama<br>
+<input type="checkbox" name="category" value="Mystery">Mystery<br>
+<input type="checkbox" name="category" value="Horror">Horror<br>
+<input type="checkbox" name="category" value="Comedy">Comedy<br>
+<input type="checkbox" name="category" value="Biographies">Biographies<br>
+<input type="checkbox" name="category" value="Autobiographies">Autobiographies<br>
+<input type="checkbox" name="category" value="Journals">Journals<br>
+<br>
+<p style="color:white;">Year Posted</p>
+<input type="checkbox" name="category" value="All">All<br>
+<input type="checkbox" name="category" value="2018">2018<br>
+<input type="checkbox" name="category" value="2017">2017<br>
+<input type="checkbox" name="category" value="2017">2016<br>
+<input type="checkbox" name="category" value="2015">2015<br>
+<input type="checkbox" name="category" value="2014">2014<br>
+<input type="checkbox" name="category" value="2013">2013<br>
+</div>
+</div>
+<div class="right">
+<div id="navbar">
+  <a href="#">Hi, Reader</a>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div>
+<img src="Images/book1.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px;">
+<img src="Images/book2.jpg" alt="book" style="width:150px; height:210px;  padding-left:10px;padding-top:10px">
+<img src="Images/book3.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book4.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book5.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book6.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book7.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book8.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book9.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book10.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book11.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book12.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book13.png" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book1.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px;">
+<img src="Images/book2.jpg" alt="book" style="width:150px; height:210px;  padding-left:10px;padding-top:10px">
+<img src="Images/book3.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book4.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book5.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book6.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book7.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book8.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book9.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book10.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book11.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book12.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book1.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px;">
+<img src="Images/book2.jpg" alt="book" style="width:150px; height:210px;  padding-left:10px;padding-top:10px">
+<img src="Images/book3.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book4.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+<img src="Images/book5.jpg" alt="book" style="width:150px; height:210px; padding-left:10px;padding-top:10px">
+
+</div>
+</div>
+</body>
+</html>
