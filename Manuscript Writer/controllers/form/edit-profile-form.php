7852724f30157fb2d4955d839fd9591809d68237
ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2018-10-21 15:48:34 +0300

    Add files via upload

--- Manuscript Writer/controllers/form/edit-profile-form.php
+++ Manuscript Writer/controllers/form/edit-profile-form.php
@@ -6,13 +6,13 @@
     </ul>
     <!-- Tab panes -->
     <div class="tab-content">
-        <div class="tab-pane fade in active" id="general">         
+        <div class="tab-pane fade in active" id="general">
             <div class="col-md-6">
-                <div class="form-group float-label-control">                      
+                <div class="form-group float-label-control">
                     <label for="">First Name</label>
                     <input type="text" class="form-control" placeholder="<?php echo $rws['user_firstname'];?>" name="user_firstname" value="<?php echo $rws['user_firstname'];?>">
                 </div>
-                <div class="form-group float-label-control">  
+                <div class="form-group float-label-control">
                     <label for="">Last Name</label>
                     <input type="text" class="form-control" placeholder="<?php echo $rws['user_lastname'];?>" name="user_lastname" value="<?php echo $rws['user_lastname'];?>">
                 </div>
@@ -22,28 +22,26 @@
                     <div class="col-md-6">
                         <div class="shortpreview">
                             <label for="">Previous Avatar </label>
-                            <br> 
+                            <br>
                             <img src="userfiles/avatars/<?php echo $rws['user_avatar'];?>" class="img-responsive">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="shortpreview" id="uploadImagePreview">
                             <label for="">Current Uploaded Avatar </label>
-                            <br> 
+                            <br>
                             <div id="imagePreview"></div>
                         </div>
                     </div>
                 </div>
-            </div>  
+            </div>
             <div class="col-md-6">
                 <label for="">Username</label>
                 <div class="form-group float-label-control">
-                    <a href="http://<?php echo $rws['domain_websiteaddress'];?>/<?php echo $rws['user_username'];?>">        
-                        <div class="input-group">
-                            <span class="input-group-addon">http://<?php echo $rws['domain_websiteaddress'];?>/</span>
-                            <fieldset disabled> 
+                                          <div class="input-group">
+                            <fieldset disabled>
                                 <input type="text" class="form-control" placeholder="<?php echo $rws['user_username'];?>" name="user_username" value="<?php echo $rws['user_username'];?>" id="disabledTextInput" autocomplete="off">
-                            </fieldset>  
+                            </fieldset>
                         </div>
                     </a>
                 </div>
@@ -52,9 +50,9 @@
                     <input type="password" class="form-control" placeholder="<?php echo $rws['user_password'];?>" name="user_password" value="<?php echo $rws['user_password'];?>">
                 </div>
                 <div class="form-group float-label-control">
-                    <label for="">Email</label> 
+                    <label for="">Email</label>
                     <input type="text" class="form-control" placeholder="<?php echo $rws['user_email'];?>" name="user_email" value="<?php echo $rws['user_email'];?>">
-                </div>  
+                </div>
             </div>
         </div>
         <div class="tab-pane fade" id="personal">
@@ -64,19 +62,19 @@
                     <textarea class="form-control" placeholder="<?php echo $rws['user_shortbio'];?>" rows="10" placeholder="<?php echo $rws['user_shortbio'];?>" name="user_shortbio" value="<?php echo $rws['user_shortbio'];?>"><?php echo $rws['user_shortbio'];?></textarea>
                 </div>
                 <div class="form-group float-label-control">
-                    <label for="">Birthday</label>   
-                    <input type="date" class="form-control" placeholder="<?php echo $rws['user_dob'];?>" name="user_dob" value="<?php echo $rws['user_dob'];?>">      
+                    <label for="">Birthday</label>
+                    <input type="date" class="form-control" placeholder="<?php echo $rws['user_dob'];?>" name="user_dob" value="<?php echo $rws['user_dob'];?>">
                 </div>
                 <div class="form-group float-label-control">
                     <label for="">Profession</label>
-                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_profession'];?>" name="user_profession" value="<?php echo $rws['user_profession'];?>" id="profession">    
-                </div>  
-                <label for="">Gender</label>              
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_profession'];?>" name="user_profession" value="<?php echo $rws['user_profession'];?>" id="profession">
+                </div>
+                <label for="">Gender</label>
                 <div class="form-group float-label-control">
                     <div class="radio-inline">
                         <label>
                             <input type="radio" name="user_gender" id="optionsRadios1" value="Male" checked>Male</label>
-                    </div>              
+                    </div>
                     <div class="radio-inline">
                         <label>
                             <input type="radio" name="user_gender" id="optionsRadios1" value="Female">Female</label>
@@ -84,24 +82,24 @@
                 </div>
                 <div class="form-group float-label-control">
                     <label for="">Country</label>
-                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_country'];?>" name="user_country" value="<?php echo $rws['user_country'];?>" id="country">    
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_country'];?>" name="user_country" value="<?php echo $rws['user_country'];?>" id="country">
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group float-label-control">
                     <label for="">Address</label>
-                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_address'];?>" name="user_address" value="<?php echo $rws['user_address'];?>">    
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_address'];?>" name="user_address" value="<?php echo $rws['user_address'];?>">
                 </div>
                 <label for="">Website</label>
                 <div class="form-group float-label-control">
-                    <div class="input-group">                  
+                    <div class="input-group">
                         <span class="input-group-addon">http://</span>
-                        <input type="text" class="form-control" placeholder="<?php echo $rws['user_website'];?>" name="user_website" value="<?php echo $rws['user_website'];?>">                  
+                        <input type="text" class="form-control" placeholder="<?php echo $rws['user_website'];?>" name="user_website" value="<?php echo $rws['user_website'];?>">
                     </div>
-                </div> 
+                </div>
             </div>
         </div>
-    </div>     
+    </div>
     <br>
     <div class="submit">
         <center>
