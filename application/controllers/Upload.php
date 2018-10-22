373fae7730d9be29d4e16986c6a0b0e67c6dff69
mujtaba098 <40717159+mujtaba098@users.noreply.github.com>
2018-10-22 23:48:07 +0300

    Add files via upload

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -16,24 +16,28 @@ class Upload extends CI_Controller {
     public function do_upload(){
         
         
-        $data['title'] = $this->input->post('title');
-        $data['category'] = $this->input->post('category');
-        $data['synopsis'] = $this->input->post('synopsis');
+        $data['Book_Name'] = $this->input->post('Book_Name');
+        $data['Book_Category'] = $this->input->post('Book_Category');
+        $data['Author'] = $this->input->post('Author');
+        $data['Year'] = $this->input->post('Year');
+        $data['Synopsis'] = $this->input->post('Synopsis');
+        
         //$data['title'] = $this->input->post('title');
 
         
         $config = array(
-            'upload_path' => "./uploads/",
+            'upload_path' => "./assets/Books/",
             'allowed_types' => "gif|jpg|png|jpeg|pdf",
             'overwrite' => TRUE,
-            'max_size' => "2048000", // Can be set to particular file size , here it is 2 MB(2048 Kb)
+            'max_size' => "4096000", // Can be set to particular file size , here it is 2 MB(2048 Kb)
             
         );
+        
         
         $this->load->library('upload', $config);
         
         
-        if ( ! $this->upload->do_upload('userfile')){
+        if ( ! $this->upload->do_upload('Book')){
 				$error = array('error' => $this->upload->display_errors());
 				$this->load->view('writer', $error);
 			}else{
@@ -43,13 +47,32 @@ class Upload extends CI_Controller {
 				$upload_data = $this->upload->data();
  
 				//get the uploaded file name
-				$data['userfile'] = $upload_data['file_name'];
+				$data['Book'] = $upload_data['file_name'];
  
 				//store pic data to the db
 				$this->upload_model->store_pic_data($data);
  
 				redirect('/');
 			}
+        
+//        if ( ! $this->upload->do_upload('Image_Data')){
+//				$error = array('error' => $this->upload->display_errors());
+//				$this->load->view('writer', $error);
+//			}else{
+// 
+//				//file is uploaded successfully
+//				//now get the file uploaded data 
+//				$upload_data = $this->upload->data();
+// 
+//				//get the uploaded file name
+//				$data['Image_Data'] = $upload_data['file_name'];
+// 
+//				//store pic data to the db
+//				$this->upload_model->store_pic_data($data);
+// 
+//				redirect('/');
+//			}
+        
         
 //        if($this->upload->do_upload())
 //        {
@@ -65,6 +88,9 @@ class Upload extends CI_Controller {
 //            $this->load->view('writer', $error);
 //            }
         }
+    
+    
+    
     }
 
 ?>
