1879febeb3fa528c7998986acf93df505093bbaa
mujtaba098 <40717159+mujtaba098@users.noreply.github.com>
2018-10-22 23:48:52 +0300

    Add files via upload

--- application/views/writer.php
+++ application/views/writer.php
@@ -30,15 +30,23 @@
   <div class="manusub">
   
 <!--  <form action="" method="post" id="upload_form">-->
-<?php echo $error;?>
   
   <?php echo form_open_multipart('upload/do_upload'); ?>
    
     <div style="padding-bottom: 18px;">Title<br/>
-<input type="text" id="title" name="title" value="title" style="width : 400px;"/>
+<input type="text" id="Book_Name" name="Book_Name" value="" style="width : 400px;"/>
 </div>
+
+<div style="padding-bottom: 18px;">Author<br/>
+<input type="text" id="Author" name="Author" value="" style="width : 400px;"/>
+</div>
+
+<div style="padding-bottom: 18px;">Year<br/>
+<input type="text" id="Year" name="Year" value="" style="width : 400px;"/>
+</div>
+
 <div style="padding-bottom: 18px;">Category<span style="color: red;"> *</span><br/>
-<select class="s" id="category" name="category" style="width : 250px;" class="form-control">
+<select class="s" id="Book_Category" name="Book_Category" style="width : 250px;" class="form-control">
  
 <option value="sci-fi">Sci-Fi</option>
 <option value="biography">Biography</option>
@@ -47,17 +55,31 @@
 <option value="coursebook">CourseBooks</option>
 <option value="action">Action</option>
 <option value="history">History</option>
+<option value="drama">Drama</option>
+<option value="thriller">Thriller</option>
+<option value="romance">Romance</option>
 
-</select></div>
+</select>
+</div>
+
 <div style="padding-bottom: 18px;">Synopsis<br/>
-<textarea id="synopsis" name="synopsis" style="width : 400px;" rows="6" class="form-control"></textarea>
-</div><br /><br />
-  <div class="upload">
+<textarea id="Synopsis" name="Synopsis" style="width : 400px;" rows="6" class="form-control"></textarea>
+</div>
+ 
+ <br /><br />
+ 
+    <div class="upload" style="padding-top:45px;">
 
-   <input type = "file" name="userfile" /><br /><br />
-   <input type = "submit" name="upload" id="upload" value="upload"/>
+  Upload Manuscript:
+   <input type = "file" name="Book" /><br /><br />
+   Upload Manuscript Cover Image:
+   <input type = "file" name="Image_Data"/><br/><br/>
+   
+   <input type = "submit" name="upload" id="upload" value="upload">
 
   </div>
+     
+     
       <?php echo "</form>" ?>
     </div>
 </body>
