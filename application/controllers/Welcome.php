3bd412d551e301ccb9418b9a585885ca765298ef
mujtaba098 <40717159+mujtaba098@users.noreply.github.com>
2018-10-22 16:16:55 +0300

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,12 +25,65 @@ class Welcome extends CI_Controller {
     
     public function index()
 	{
-		$this->load->view('publisher');
-       // $this->load->helper('');
+       $this->load->model('Bookmodel');
+      $records = $this->Bookmodel->getBooks();
+      //$this->load->helper('url');
+      $this->load->view('publisher', ['records' =>$records]);
+
 	}
+    
+     public function getCover(){
+   $this->load->model('bookmodel');
+      $records = $this->bookmodel->getRecords();
+      //$this->load->helper('url');
+		$this->load->view('read', ['records' =>$records]);
+       // $this->load->helper('');
+     
+    }
+    
+    public function SearchResults(){
+
+      $this->load->model('Bookmodel');
+      $records = $this->Bookmodel->search();
+      //$this->load->helper('url');
+      $this->load->view('publisher', ['records' =>$records]);
+
+    }
+    
+     public function Category(){
+
+      $this->load->model('Bookmodel');
+      $records = $this->Bookmodel->getCategory();
+      //$this->load->helper('url');
+      $this->load->view('publisher', ['records' =>$records]);
+
+    }
+    
+    public function rate(){
+
+  $id=$_GET["id"];
+  $rating=$_POST["rating"];
+  $comment=$_POST["comments"];
+  $rating= array(
+    'User_ID' => 1,
+    'Book_ID' =>$id,
+    'Rating' =>$rating,
+    'Comment' =>$comment   
+  );
+        
+   $this->load->model('Bookmodel');
+   $this->Bookmodel->RateBook($rating);
+   $this->load->model('Bookmodel');
+      $records = $this->Bookmodel->getRecords();
+      //$this->load->helper('url');
+      $this->load->view('read', ['records' =>$records]);
+
+ }
     
     function reader(){
         $this->load->view('readers');
     }
+    
+    
 
 }
