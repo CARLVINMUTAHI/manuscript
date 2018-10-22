4d910f4daf0cffeca5c642c2970f90408a7ef072
ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2018-10-22 15:02:04 +0300

    Add files via upload

---
+++ Manuscript Writer/uploads.php
@@ -0,0 +1,6 @@
+<?php include 'components/authentication.php' ?>
+<?php include 'controllers/base/head.php' ?>
+<?php include 'controllers/navigation/first-navigation.php' ?>
+<?php
+?>
+<?php include 'controllers/form/upload-manuscript-form.php' ?>
